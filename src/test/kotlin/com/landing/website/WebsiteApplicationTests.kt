27272da734067a15ec46a3cba681fb3ac27eafdf
fz on desk <tom@bkfam.nl>
2025-04-25 20:08:35 +0200

    init

---
+++ src/test/kotlin/com/landing/website/WebsiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.landing.website
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
